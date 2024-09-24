f5e69cccef9032fd13c7b3f268c185265cca5741
NicolasScaglia <Nicolas.Scaglia@alu.uclm.es>
2024-09-24 14:16:37 +0200

    Fin de la clase

--- busqueda_ciudades.py
+++ busqueda_ciudades.py
@@ -9,23 +9,33 @@ def loadData(fileName):
         print("El fichero no es un .json")
         return
 
-
-#class Ubicacion:
-
-#class Interseccion:
-
-#class MoverseAInterseccion:
-
-#class ElegirInterseccion:
-
 def toMetersPerSecond(kilometersPerHour):
     return (kilometersPerHour * 1000) / 3600
+
+class Interseccion:
+
+    def __init__(self, id, long, lat):
+        self.identificacion = id
+        self.longitud = long
+        self.latitud = lat
+
+class Seccion:
+
+    def __init__(self, origen, dest, dist, vel):
+        self.origen = origen
+        self.destino = dest
+        self.peso = dist / toMetersPerSecond(vel)
+
+
 
 def main():
     data = loadData("problems/small/avenida_de_espanÌa_250_0.json")
     if not data:
         return
-    print(data['address'])
+    intersecciones = []
+    for element in data['intersections']:
+        intersecciones.append(Interseccion(element['identifier'], element['longitude'], element['latitude']))
+    
 
 if __name__ == "__main__":
     main()
