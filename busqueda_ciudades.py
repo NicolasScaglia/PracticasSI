9deb64fb899450000f38297c6d4585084c8263cf
NicolasScaglia <128148438+NicolasScaglia@users.noreply.github.com>
2024-10-01 16:51:21 +0200

    Segunda Clase

--- busqueda_ciudades.py
+++ busqueda_ciudades.py
@@ -1,13 +1,54 @@
 import json
 
 def loadData(fileName):
-    if fileName.endswith(".json"):
-        with open(fileName, 'r') as file:
-            data = json.load(file)
-        return data
-    else:
-        print("El fichero no es un .json")
+    if not fileName.endswith(".json"):
+        print("El archivo no es válido")
         return
+    with open(fileName, 'r') as file:
+        data = json.load(file)
+    return data
+
+class Estado:
+
+    def __init__(self, id, long, lat):
+        self.identificador = id
+        self.longitud = long
+        self.latitud = lat
+class Accion:
+
+    def __init__(self, orig, dest, dist, vel):
+        self.origen = orig
+        self.destino = dest
+        self.coste = dist / toMetersPerSecond(vel)
+
+class Nodo:
+
+    def __init__(self, est, pad, acc, prof, cost):
+        self.estado = est
+        self.padre = pad
+        self.accion = acc
+        self.profundiad = prof
+        self.coste = cost
+
+class Problema:
+
+
+    def __init__(self, problemName):
+        self.problemName = problemName
+        data = loadData(self.problemName)
+        if not data:
+            return
+        self.conexiones = self.calcularAcciones(data)
+
+    def calcularAcciones(self, data):
+        conexiones = {}
+        for elements in data['segments']:
+            if elements['origin'] not in conexiones:
+                conexiones[elements['origin']] = []
+            conexiones[elements['origin']].append(elements['destination'])
+        return conexiones
+    
+    
 
 def toMetersPerSecond(kilometersPerHour):
     return (kilometersPerHour * 1000) / 3600
@@ -29,12 +70,8 @@ class Seccion:
 
 
 def main():
-    data = loadData("problems/small/avenida_de_espanÌa_250_0.json")
-    if not data:
-        return
-    intersecciones = []
-    for element in data['intersections']:
-        intersecciones.append(Interseccion(element['identifier'], element['longitude'], element['latitude']))
+    prob = Problema()
+    
     
 
 if __name__ == "__main__":
