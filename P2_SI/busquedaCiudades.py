49e3d4875227098ff83ea1f04f4f55eeb3c692ac
NicolasScaglia <Nicolas.Scaglia@alu.uclm.es>
2024-12-12 19:24:42 +0100

    Preparación para Segunda práctica.

--- P2_SI/busquedaCiudades.py
+++ P2_SI/busquedaCiudades.py
@@ -1,10 +1,7 @@
 import json
 from queue import PriorityQueue
-import datetime
 from geopy import distance
-from timeit import default_timer as timer
 from abc import ABC, abstractmethod
-import random
 
 
 def load_data(fileName):
@@ -56,31 +53,24 @@ class Problema:
         if not self.data:
             print("[ERROR] No se ha encontrado la información del problema.")
             return
-        self.num_estaciones = self.data['number_stations']
-        self.candidatos = self.data['candidates']
+        # Encontramos el estado final y el estado inicial en el diccionario de estados
         self.calcular_acciones()
         self.calcular_estados()
-
-    def set_estado_final(self, estado):
-        self.estado_final = estado
-    def set_estado_inicial(self, estado):
-        self.estado_inicial = estado
+        self.candidatos = self.data['candidates']
+        self.numero_estaciones = self.data['number_stations']
     
     # Diccionario de acciones para calcular las conexiones entre intersecciones
     def calcular_acciones(self):
         self.acciones = {}
-        # self.velocaidad_media = 0
         self.velocidad_maxima = 0
-        # cuenta = 0
-        self.data['segments'].sort(key = lambda a : a['destination'])
+        self.data['segments'].sort()
         for element in self.data['segments']:
             if element['origin'] not in self.acciones:
-                self.acciones[element['origin']] = []
-            self.acciones[element['origin']].append(Accion(element['origin'], element['destination'], element['distance'], element['speed']))
+                self.acciones[element['origin']].put((element['destination'],Accion(element['origin'], element['destination'], element['distance'], element['speed'])))
             if element['speed'] > self.velocidad_maxima:
                 self.velocidad_maxima = element['speed']
-        # self.velocaidad_media /= cuenta
-        
+
+
     # Diccionario de estados, id del estado, estado en el otro lado
     def calcular_estados(self):
         self.estados = {}
@@ -96,37 +86,25 @@ class Heuristica():
 
 class Busqueda(ABC):
 
-    generados = 0
-    expandidos = 0
-    explorados = 0
-    coste = 0
     
     def __init__(self, problema, frontera):
         self.problema = problema
         self.frontera = frontera
 
     def start(self):
-        start = timer() 
         self.solucion = self.algoritmo()
-        if self.solucion is not None:
-            self.coste = self.solucion.coste
-        end = timer()
-        self.tiempoEjecucion = end - start
-        #self.problema.calcular_acciones()
         
     def algoritmo(self):
         self.cerrados = set()
-        nodoInicial = Nodo(self.problema.estado_inicial, None, None)
+        nodoInicial = Nodo(self.problema.estadoInicial, None, None)
         self.insertar(nodoInicial)
         while(True):
             if self.es_vacia():
-                self.nodoActual.coste = float('inf')
-                return self.nodoActual
+                self.nodoActual.coste = 3600*5
+                return self.nodoActual.coste
             self.nodoActual = self.sacar_siguiente()
             if self.es_final():
-                self.explorados += 1
-                return self.nodoActual
-            self.explorados += 1
+                return self.nodoActual.coste
             if self.nodoActual.estado not in self.cerrados:
                 self.cerrados.add(self.nodoActual.estado)
                 self.abrir_nodo()
@@ -143,10 +121,9 @@ class Busqueda(ABC):
         pass    
 
     def es_final(self):
-        return self.nodoActual.estado == self.problema.estado_final
+        return self.nodoActual.estado == self.problema.estadoFinal
 
     def abrir_nodo(self):
-        self.expandidos += 1
         if self.nodoActual.estado.identificador in self.problema.acciones:
             acciones = self.problema.acciones[self.nodoActual.estado.identificador]
         else:
@@ -155,8 +132,7 @@ class Busqueda(ABC):
             accion = element
             nodoFrontera = Nodo(self.problema.estados[accion.destino], self.nodoActual, accion)
             self.insertar(nodoFrontera)
-            self.generados += 1
-
+            
 class AE(Busqueda):
 
     def __init__(self, problema, heuristica):
@@ -166,199 +142,11 @@ class AE(Busqueda):
 
     def insertar(self, nodo):
         posicion_actual = (nodo.estado.latitud, nodo.estado.longitud)
-        prioridad = self.heuristica.funcion_heuristica(posicion_actual, (self.problema.estado_final.latitud, self.problema.estado_final.longitud)) + nodo.coste
+        prioridad = self.heuristica.funcion_heuristica_geodesica(posicion_actual, self.problema.posicionFinal) + nodo.coste
         self.frontera.put((prioridad, nodo))
-        
+
     def sacar_siguiente(self):
         return self.frontera.get()[1]
-
-def limitar_seleccionados(cadena, seleccionados):
-    if cadena.count(1) == seleccionados:
-        return cadena;
-    elif cadena.count(1) > seleccionados:
-        indices = [i for i, bit in enumerate(cadena) if bit == 1]
-        exceso = len(indices) - seleccionados
-        cambios = random.sample(indices, exceso)
-        for i in cambios:
-            cadena[i] = 0
-        return cadena
-    else:
-        indices = [i for i, bit in enumerate(cadena) if bit == 0]
-        faltantes = seleccionados - cadena.count(1)
-        cambios = random.sample(indices, faltantes)
-        for i in cambios:
-            cadena[i] = 1
-        return cadena
-            
-class Individuo():
-
-    # Cadena genética representa la solución del individuo.
-    # Tamaño representa el tamaño de la cadena genética.
-    # Todo individuo tiene un valor de fitness, o lo que es lo mismo,
-    # un valor de aptitud.
-    def __init__(self, cadena_genetica=None, tamano=1, seleccionados=1):
-        if cadena_genetica is None:
-            # Si no se pasa una cadena genética en concreto, usamos el resto de parámetros
-            # siempre y cuando sean válidos para generar una.
-            if tamano < 1 or seleccionados < 1:
-                return
-            self.seleccionados = seleccionados
-            self.tamano = tamano
-            # Inicializamos la cadena genética sin ninguna selección.
-            self.cadena_genetica = [0] * tamano
-            # La función generar hace una selección aleatoria de @seleccionados elementos.
-            self.generar()
-        else:
-            # Si pasan una cadena genética, obtenemos los parámetros de ésta.
-            self.cadena_genetica = cadena_genetica
-            self.tamano = len(self.cadena_genetica)
-            self.seleccionados = self.cadena_genetica.count(1)
-
-        self.fitness = 0
-
-    def evaluar(self, problema):
-        # Evaluamos al individuo usando la función de evaluación
-        sumPop = 0
-        min = -112
-        for element in problema.candidatos:
-            for i in range(0, len(self.cadena_genetica)):
-                if self.cadena_genetica[i] == 1:
-                    problema.set_estado_inicial(problema.estados[element[0]])
-                    problema.set_estado_final(problema.estados[problema.candidatos[i][0]])
-                    estrella = AE(problema, Heuristica(problema.velocidad_maxima))
-                    estrella.start()
-                    if estrella.solucion.coste * element[1] < min or min < 0:
-                        min = estrella.solucion.coste * element[1]
-            sumPop += element[1]
-        self.fitness = (1/sumPop) * min
-
-    # Función mutable
-    def cruzar(self, otro, probabilidad=1.0):
-
-        if random.random() < probabilidad:
-            cruce = random.randint(1, len(self.cadena_genetica) - 1)
-            hijo1 = otro.cadena_genetica[cruce:] + self.cadena_genetica[:cruce]
-            hijo2 = self.cadena_genetica[cruce:] + otro.cadena_genetica[:cruce]
-
-            hijo1 = Individuo(cadena_genetica=limitar_seleccionados(hijo1, self.seleccionados))
-            hijo2 = Individuo(cadena_genetica=limitar_seleccionados(hijo2, self.seleccionados))
-            
-        else:
-            hijo1 = Individuo(cadena_genetica=self.cadena_genetica.copy())
-            hijo2 = Individuo(cadena_genetica=otro.cadena_genetica.copy())
-
-        return hijo1, hijo2
-
-    def mutar(self, probabilidad=1.0):
-        # Aplica una mutación a partir de una probabildad dada.
-        for gen in self.cadena_genetica:
-            if random.random() < probabilidad:
-                gen = 1 - gen
-        self.cadena_genetica = limitar_seleccionados(self.cadena_genetica, self.seleccionados)
-
-    def generar(self):
-        # Mientras que no haya el número de 1s (seleccionados) igual a
-        # el número de seleccionados que necesitamos, continuamos.
-        while self.cadena_genetica.count(1) != self.seleccionados:
-            pos = random.randint(0, len(self.cadena_genetica) - 1)
-            self.cadena_genetica[pos] = 1
-
-    def __eq__(self, otro):
-        return self.cadena_genetica.__eq__(otro.cadena_genetica)
-
-
-class AlgoritmoAleatorio():
-
-
-    # Se debe pasar el problema en concreto y el tamaño de la población
-    # al algoritmo.
-    def __init__(self, problema, tamano_poblacion = 100):
-        self.problema = problema
-        self.tamano_poblacion = tamano_poblacion
-
-        # Utilizamos una caché para que si ya tenemos un resultado, no volver
-        # a realizar una evaluación innecesaria. Key = tupla(cadena_genetica) ,
-        # Value = fitness 
-        self.cache = {}
-    
-    def algoritmo(self):
-        mejor_individuo = None
-        contadorOptimoLocal = 0
-
-        # Comprobamos que hayamos alcanzado al menos un óptimo local
-        # (las últimas tres soluciones sean iguales)
-
-        # Inicializamos el mejor individuo local a nulo en cada iteración.
-        mejor_individuo_local = None
-        while contadorOptimoLocal < 3:
-        # Hacemos un bucle que dure el tamaño de la población.
-            for i in range(self.tamano_poblacion):
-
-                # Generamos el individuo en el momento en vez de guardarlo en un array, así
-                # ahorramos memoria.
-                temp = Individuo(tamano=len(self.problema.candidatos), seleccionados=self.problema.num_estaciones)
-
-                # Variable fitness temporal.
-                fitness = 0
-
-                # Acceso a la caché (simplifica la lectura del código).
-                acceso = tuple(temp.cadena_genetica)
-
-                # Si existe valor en caché, usamos ese y no evaluamos de nuevo.
-                if acceso in self.cache and self.cache[acceso] is not None:
-                    fitness = self.cache[acceso]
-                else:
-                    # Si no, fitness será el valor de la evaluación y la guardamos en caché.
-                    temp.evaluar(self.problema)
-                    fitness = temp.fitness
-                    self.cache[acceso] = fitness
-
-                # Actualizamos el mejor valor.
-                if mejor_individuo_local is None:
-                    mejor_individuo_local = temp
-                
-                if mejor_individuo_local.fitness < fitness:
-                    mejor_individuo_local = temp
-                
-                # Devolvemos el mejor individuo.
-            if mejor_individuo is None:
-                mejor_individuo = mejor_individuo_local
-            elif mejor_individuo_local.fitness > mejor_individuo.fitness:
-                mejor_individuo = mejor_individuo_local
-                contadorOptimoLocal = 0
-            elif mejor_individuo_local.__eq__(mejor_individuo):
-                contadorOptimoLocal += 1
-
-        return mejor_individuo_local
-
-
-# class AlgoritmoGenetico():
-
-#     # Se debe pasar el problema en concreto y el tamaño de la población
-#     # inicial al algoritmo.
-#     def __init__(self, problema, tamano_poblacion_inicial=100):
-#         self.problema = problema
-#         self.tamano_poblacion = tamano_poblacion_inicial
-
-#         # Utilizamos una caché para que si ya tenemos un resultado, no volver
-#         # a realizar una evaluación innecesaria. Key = tupla(cadena_genetica) ,
-#         # Value = fitness 
-#         self.cache = {}
-
-#     def algoritmo(self):
-#         poblacion = [Individuo(tamano=len(self.problema.candidatos), seleccionados=self.problema.num_estaciones)] * self.tamano_poblacion
-#         condicion_de_parada = False
-#         t = 0
-#         ind = poblacion[t]
-#         ind.evaluar()
-#         # Selección basada en rango
-#         while not condicion_de_parada:
-#             t += 1
-#             # P(t)'
-#             indp = poblacion[t]
-#             cruces = ind.cruzar(indp)
-
-
 
 # def imprimirResultado(busqueda):
 #     print(f"Nodos generados: {busqueda.generados}")
@@ -370,33 +158,23 @@ class AlgoritmoAleatorio():
 #     print(f"Coste final: {coste}")
 #     reconstruirCamino(busqueda.solucion)
 
-# # def reconstruirCamino(nodo):
-# #     if nodo is None:
-# #         return
-# #     ids = [nodo.estado.identificador]
-# #     while nodo.padre is not None:
-# #         nodo = nodo.padre
-# #         ids.append(nodo.estado.identificador)
-# #     ids.reverse()
-# #     print(f"Longitud de la solucion: {len(ids)}")
-# #     print(f"Camino recorrido: {ids}")
-
-def imprimir_solucion(solucion, candidatos):
-
-    aux = []
-    for i in range(len(solucion)):
-        if solucion[i] == 1:
-            aux.insert(0, candidatos[i][0])
-    print(aux)
+# def reconstruirCamino(nodo):
+#     if nodo is None:
+#         return
+#     ids = [nodo.estado.identificador]
+#     while nodo.padre is not None:
+#         nodo = nodo.padre
+#         ids.append(nodo.estado.identificador)
+#     ids.reverse()
+#     print(f"Longitud de la solucion: {len(ids)}")
+#     print(f"Camino recorrido: {ids}")
 
 def toMetersPerSecond(kilometersPerHour):
     return (kilometersPerHour * 1000) / 3600
 
 def main():
-    prob = Problema("M:\\University\\PracticasSI\\P2_SI\\sample-problems-lab2\\toy\\calle_del_virrey_morcillo_albacete_250_3_candidates_15_ns_4.json")
-    AA = AlgoritmoAleatorio(prob, 100)
-    solucionAA = AA.algoritmo()
-    imprimir_solucion(solucionAA.cadena_genetica, prob.candidatos)
+    prob = Problema("sample-problems-lab2/toy/calle_del_virrey_morcillo_albacete_250_3_candidates_15_ns_4.json")
 
 if __name__ == "__main__":
     main()
+
