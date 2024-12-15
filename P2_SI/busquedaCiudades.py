03e634a46128f1062924bc1c68d2eeaa6115260b
NicolasScaglia <Nicolas.Scaglia@alu.uclm.es>
2024-12-15 20:26:58 +0100

    Finalización P2.

--- P2_SI/busquedaCiudades.py
+++ P2_SI/busquedaCiudades.py
@@ -152,14 +152,17 @@ class AE(Busqueda):
 class Individuo():
 
     cache = {}
+    hits = 0
+    misses = 0
 
     def __init__(self, seleccionados=None, candidatos=[], tamano=0):
         if seleccionados is None:
-            self.seleccionados = set()
-            self.candidatos = candidatos
-            self.tamano = tamano
+            self.seleccionados = tuple()
+            self.generar(candidatos, tamano)
         else:
-            self.seleccionados = set(seleccionados)
+            self.seleccionados = tuple(seleccionados)
+        self.candidatos = candidatos
+        self.tamano = tamano
 
     def evaluar(self, problema):
         sumPop = 0
@@ -172,49 +175,53 @@ class Individuo():
                 tupla = (candidato[0], solucion)
                 if tupla in Individuo.cache:
                     val = Individuo.cache[tupla]
+                    Individuo.hits += 1
                 else:
                     estrella.problema.estado_inicial = problema.estados[candidato[0]]
                     estrella.problema.estado_final = problema.estados[solucion]
                     estrella.start()
                     val = estrella.solucion
                     Individuo.cache[tupla] = val
+                    Individuo.misses += 1
                 minimum = min(val, minimum)
             sumPop += candidato[1]
             aux += candidato[1]*minimum
-        self.fitness = (1/sumPop) * aux  
+        self.fitness = (1/sumPop) * aux
 
     def cruzar(self, otro, probabilidad=1.0):
 
         if random.random() < probabilidad:
             corte = random.randint(1, len(self.seleccionados) - 1)
             piscina_genetica = self.seleccionados + otro.seleccionados
-            hijo1 = set(self.seleccionados[corte:] + otro.seleccionados[:corte])
-            hijo2 = set(otro.seleccionados[corte:] + self.seleccionados[:corte])
+            hijo1 = list(self.seleccionados[corte:] + otro.seleccionados[:corte])
+            hijo2 = list(otro.seleccionados[corte:] + self.seleccionados[:corte])
             if len(hijo1) < len(self.seleccionados):
                 faltantes = len(self.seleccionados) - len(hijo1)
-                hijo1.add(x for x in random.sample(piscina_genetica - hijo1, faltantes))
+                hijo1.append(x for x in random.sample(piscina_genetica - hijo1, faltantes))
             if len(hijo2) < len(self.seleccionados):
                 faltantes = len(self.seleccionados) - len(hijo2)
-                hijo1.add(x for x in random.sample(piscina_genetica - hijo2, faltantes))
+                hijo1.append(x for x in random.sample(piscina_genetica - hijo2, faltantes))
         else:
             hijo1 = self.seleccionados
             hijo2 = otro.seleccionados
 
-        return (hijo1, hijo2)
+        return (Individuo(hijo1, candidatos=self.candidatos), Individuo(hijo2, candidatos=self.candidatos))
                     
     def mutar(self, probabilidad=1.0):
         # Aplica una mutación a partir de una probabildad dada.
-        for i in range(len(self.seleccionados)):
+        aux = list(self.seleccionados)
+        for _ in range(len(self.seleccionados)):
             if random.random() < probabilidad:
                 nuevo = random.choice(self.candidatos)[0]
-                while nuevo in self.seleccionados:
+                while nuevo in aux:
                     nuevo = random.choice(self.candidatos)[0]
-                self.seleccionados.remove(random.choice(self.seleccionados))
-                self.seleccionados.add(nuevo)
+                aux.remove(random.choice(aux))
+                aux.append(nuevo)
+        self.seleccionados = tuple(aux)
     
-    def generar(self):
-        list = random.sample(self.candidatos, self.tamano)
-        self.seleccionados = set(x[0] for x in list)
+    def generar(self, candidatos, tamano):
+        list = random.sample(candidatos, tamano)
+        self.seleccionados = tuple(x[0] for x in list)
 
 
 class AlgoritmoAleatorio():
@@ -230,9 +237,8 @@ class AlgoritmoAleatorio():
 
         for i in range(self.tamano_poblacion):
             temp = Individuo(candidatos=self.problema.candidatos, tamano=self.problema.numero_estaciones)
-            temp.generar()
 
-            acceso = tuple(temp.seleccionados)
+            acceso = temp.seleccionados
 
             if acceso in AlgoritmoAleatorio.cache:
                 fitness = AlgoritmoAleatorio.cache[acceso]
@@ -250,7 +256,133 @@ class AlgoritmoAleatorio():
         return mejor_individuo
 
 
+class AlgoritmoGenetico:
+
+    cache = {}
+
+    def __init__(self, problema, tamano_poblacion=100, generaciones=50):
+        self.problema = problema
+        self.tamano_poblacion = tamano_poblacion
+        self.generaciones = generaciones
+
+    def seleccionar_rango(self, poblacion):
+
+        N = len(poblacion)
+        seleccionados = []
+
+        prob = [2 * (N - i + 1) / (N**2 + N) for i in range(0, N)]
+
+        seleccionados = random.choices(population=poblacion, weights=prob, k=random.randint(5, N))
+
+        return seleccionados
+
+    def seleccionar_torneo(self, poblacion, k=2):
+        seleccionados = []
+        for _ in range(len(poblacion)):
+            torneo = random.sample(poblacion, k)
+            ganador = max(torneo, key=lambda i : i.fitness)
+            seleccionados.append(ganador)
+        return seleccionados
+
+
+
+    def algoritmo(self):
+
+
+        condicion_de_parada = False
+
+        convergencia = float('inf')
+
+        anterior = None
+        
+        #t = 0
+        numero_de_generaciones = 0
+
+
+        mejor_individuo = None
+
+
+        # inicializar P(t)
+        poblacion = [Individuo(candidatos=self.problema.candidatos, tamano=self.problema.numero_estaciones) for _ in range(self.tamano_poblacion) ]
+
+        # evaluar P(t)
+        for element in poblacion:
+            acceso = element.seleccionados
+            if acceso in AlgoritmoGenetico.cache:
+                element.fitness = AlgoritmoGenetico.cache[acceso]
+            else:
+                element.evaluar(self.problema)
+                AlgoritmoGenetico.cache[acceso] = element.fitness
+        
+        poblacion.sort(key=lambda i : i.fitness, reverse=True)
+
+        while not condicion_de_parada:
+            numero_de_generaciones += 1
+            # poblacion_auxiliar = self.seleccionar_torneo(poblacion, random.randint(1, self.tamano_poblacion))
+            poblacion_auxiliar = self.seleccionar_rango(poblacion)
+
+            nueva_poblacion = []
+
+            N = self.tamano_poblacion
+            for i in range(0, len(poblacion_auxiliar), 2):
+                if i + 1 < len(poblacion_auxiliar):
+                    
+                    hijos = poblacion_auxiliar[i].cruzar(poblacion_auxiliar[i+1], probabilidad=2 * (N - i + 1) / (N**2 + N))
+                    hijo1 = hijos[0]
+                    hijo2 = hijos[1]
+
+                    hijo1.mutar()
+                    hijo2.mutar()
+
+                    acceso1 = hijo1.seleccionados
+                    if acceso1 in AlgoritmoGenetico.cache:
+                        hijo1.fitness = AlgoritmoGenetico.cache[acceso1]
+                    else:
+                        hijo1.evaluar(self.problema)
+                        AlgoritmoGenetico.cache[acceso1] = hijo1.fitness
+
+                    acceso2 = hijo2.seleccionados
+                    if acceso2 in AlgoritmoGenetico.cache:
+                        hijo2.fitness = AlgoritmoGenetico.cache[acceso2]
+                    else:
+                        hijo2.evaluar(self.problema)
+                        AlgoritmoGenetico.cache[acceso2] = hijo2.fitness
+
+                    nueva_poblacion.extend([hijo1, hijo2])
+
+                else:
+                    nueva_poblacion.append(poblacion_auxiliar[i])
+
+            combinada = poblacion + nueva_poblacion
+
+
+            poblacion = sorted(combinada, key= lambda i : i.fitness, reverse=True)[:self.tamano_poblacion]
+
+            if mejor_individuo is None:
+                mejor_individuo = poblacion[0]
+            elif mejor_individuo.fitness < poblacion[0].fitness:
+                mejor_individuo = poblacion[0]
+
+            if anterior is not None:
+                dif = list(set(poblacion) - anterior)
+                convergencia = len(dif)
             
+            anterior = set(poblacion)
+
+            print(f"Generación: {numero_de_generaciones}")
+            print(f"Mejor Individuo: {mejor_individuo.seleccionados}")
+            print(f"Convergencia: {convergencia}")
+            print(f"Hits en caché Individuo: {Individuo.hits}")
+            print(f"Fallos en caché Individuo: {Individuo.misses}")
+
+            condicion_de_parada = self.tamano_poblacion - convergencia >= 0.1*self.tamano_poblacion
+
+
+        return mejor_individuo
+                
+            
+
+
 
 
 # def imprimirResultado(busqueda):
@@ -281,9 +413,12 @@ def toMetersPerSecond(kilometersPerHour):
     return (kilometersPerHour * 1000) / 3600
 
 def main():
-    prob = Problema("P2_SI/sample-problems-lab2/toy/calle_del_virrey_morcillo_albacete_250_3_candidates_15_ns_4.json")
+    prob = Problema("sample-problems-lab2/huge/plaza_santa_teresa_de_jesús_jornet_albacete_2000_4_candidates_433_ns_46.json")
     AA = AlgoritmoAleatorio(prob)
     print(AA.algoritmo().seleccionados)
+    print("-------------------------------------------------------------------")
+    AG = AlgoritmoGenetico(prob, 100)
+    print(f"Solución: {AG.algoritmo().seleccionados}")
 
 if __name__ == "__main__":
     main()
